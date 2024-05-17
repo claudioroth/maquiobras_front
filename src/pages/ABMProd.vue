7c202666a0cd1f470a0cdbf5befaeac2f1874e82
Emanuel Oppedisano <sevenkowen@hotmail.com>
2024-05-16 23:27:41 -0300

    tabla producto

--- src/pages/ABMProd.vue
+++ src/pages/ABMProd.vue
@@ -38,8 +38,9 @@
       virtual-scroll
       v-model:pagination="pagination"
       :rows-per-page-options="[0]"
+      separator="cell"
       color="primary"
-      class="no-shadow text-grey-7"
+      class="no-shadow text-grey-7 my-sticky-header-last-column-table my-sticky-header-table"
       :style="`border: solid 1px #e0e0e0; height:${$q.screen.height - 190}px ;`"
     >
       <!-- <template v-slot:top-right>
@@ -50,24 +51,175 @@
         </q-input>
       </template> -->
 
-      <template v-slot:body-cell-retiro="props">
+      <!-- Venta IVA -->
+      <template v-slot:body-cell-venta_iva="props">
+        <q-td :props="props">
+            {{ props.row.venta_iva ? props.row.venta_iva : "-"  }}
+        </q-td>
+      </template>
+
+      <template v-slot:body-cell-cantidad="props">
         <q-td :props="props">
           <div>
-            <q-badge color="red-7" :label="props.row.retiro" />
+            <q-badge
+              v-if="props.row.cantidad"
+              color="red-7"
+              :label="props.row.cantidad ? props.row.cantidad : '-'"
+            />
+            <div v-else>-</div>
           </div>
         </q-td>
       </template>
 
-      <template v-slot:body-cell-fecha="props">
+      <template v-slot:body-cell-importe="props">
         <q-td :props="props">
-          <div>{{ parse_datetime(props.row.fecha, "date") }}</div>
+          <div>$ {{ props.row.importe }}</div>
+        </q-td>
+      </template>
+
+      <!-- Iva 21 -->
+      <template v-slot:body-cell-iva_21="props">
+        <q-td :props="props">
           <div>
-            <q-badge color="red-7">
-              {{ parse_datetime(props.row.fecha, "hours") }}
-            </q-badge>
+            {{  props.row.prov1 ? "$" + parse_decimal(props.row.iva_21) : "-" }}
           </div>
         </q-td>
       </template>
+
+
+      <!-- Iva 10 -->
+      <template v-slot:body-cell-iva_10="props">
+        <q-td :props="props">
+          <div>
+            {{  props.row.prov1 ? "$" + parse_decimal(props.row.iva_10) : "-" }}
+          </div>
+        </q-td>
+      </template>
+
+      <!-- Aumento -->
+      <template v-slot:body-cell-aumento="props">
+        <q-td v-if="props.row.aumento" :props="props">
+          <div>{{ parse_datetime(props.row.aumento, "date") }}</div>
+          <div>
+            <q-badge color="red-7">
+              {{ parse_datetime(props.row.aumento, "hours") }}
+            </q-badge>
+          </div>
+        </q-td>
+        <q-td v-else :props="props"> - </q-td>
+      </template>
+
+      <!-- Ultima Modificacion -->
+      <template v-slot:body-cell-ultimo_modif="props">
+        <q-td v-if="props.row.ultimo_modif" :props="props">
+          <div>{{ parse_datetime(props.row.ultimo_modif, "date") }}</div>
+          <div>
+            <q-badge color="red-7">
+              {{ parse_datetime(props.row.ultimo_modif, "hours") }}
+            </q-badge>
+          </div>
+        </q-td>
+        <q-td v-else :props="props"> - </q-td>
+      </template>
+
+      <!-- Oferta -->
+      <template v-slot:body-cell-oferta="props">
+        <q-td :props="props">
+          <div>
+             {{ props.row.oferta ? parse_decimal(props.row.oferta) : "-" }}
+          </div>
+        </q-td>
+      </template>
+
+      <!-- Proveedor1 -->
+      <template v-slot:body-cell-prov1="props">
+        <q-td :props="props">
+          <div>
+            {{ props.row.prov1 ? "$" + parse_decimal(props.row.prov1) : "-" }}
+          </div>
+        </q-td>
+      </template>
+
+      <!-- Proveedor2 -->
+      <template v-slot:body-cell-prov2="props">
+        <q-td :props="props">
+          <div>
+            {{ props.row.prov2 ? "$" + parse_decimal(props.row.prov2) : "-" }}
+          </div>
+        </q-td>
+      </template>
+
+           <!-- Proveedor3 -->
+           <template v-slot:body-cell-prov3="props">
+        <q-td :props="props">
+          <div>
+            {{ props.row.prov3 ? "$" + parse_decimal(props.row.prov3) : "-" }}
+          </div>
+        </q-td>
+      </template>
+
+      <!-- Costo Bajo -->
+      <template v-slot:body-cell-costo_bajo="props">
+        <q-td :props="props">
+          <div>
+            {{
+              props.row.costo_bajo ? "$" + parse_decimal(props.row.costo_bajo) : "-"
+            }}
+          </div>
+        </q-td>
+      </template>
+
+      <!-- Costo Bajo 1 -->
+      <template v-slot:body-cell-costo_bajo1="props">
+        <q-td :props="props">
+          <div>
+            {{
+              props.row.costo_bajo1 ? "$" + parse_decimal(props.row.costo_bajo1) : "-"
+            }}
+          </div>
+        </q-td>
+      </template>
+
+      <!-- Venta -->
+      <template v-slot:body-cell-venta="props">
+        <q-td :props="props">
+          <div>
+            {{ parse_decimal(props.row.venta) }}
+          </div>
+        </q-td>
+      </template>
+
+      <!-- Rentab -->
+      <template v-slot:body-cell-rentab="props">
+        <q-td :props="props">
+          <div>
+            <q-badge
+              v-if="props.row.rentab"
+              color="red-7"
+              :label="props.row.rentab ? props.row.rentab : '-'"
+            />
+            <div v-else>-</div>
+          </div>
+        </q-td>
+      </template>
+
+      <!-- Venta IVA -->
+      <template v-slot:body-cell-un_18="props">
+        <q-td :props="props">
+            {{ props.row.un_18 ? props.row.un_18 : "-"  }}
+        </q-td>
+      </template>
+
+            <!-- Venta Oferta -->
+      <template v-slot:body-cell-venta_oferta="props">
+        <q-td :props="props">
+          <div>
+            {{ parse_decimal(props.row.venta_oferta) }}
+          </div>
+        </q-td>
+      </template>
+
+
     </q-table>
 
     <!-- LOADING SCREEN -->
@@ -100,7 +252,7 @@ export default defineComponent({
   setup() {
     // VARIABLES
     const loadingScreen = ref(true);
-    const loadingTable = ref(false)
+    const loadingTable = ref(false);
     const dataTable = ref([]);
     const columns = [
       {
@@ -120,16 +272,23 @@ export default defineComponent({
         name: "descripcion",
         label: "Descripcion",
         field: "descripcion",
-        align: "center",
+        align: "left",
         sortable: true,
       },
       {
-        name: "lista_vieja",
-        label: "Lista Vieja",
-        field: "lista_vieja",
+        name: "cantidad",
+        label: "Cantidad",
+        field: "cantidad",
         align: "center",
         sortable: true,
       },
+      // {
+      //   name: "lista_vieja",
+      //   label: "Lista Vieja",
+      //   field: "lista_vieja",
+      //   align: "center",
+      //   sortable: true,
+      // },
       {
         name: "importe",
         label: "Importe",
@@ -269,6 +428,12 @@ export default defineComponent({
         });
     });
 
+    const parse_decimal = (value) => {
+      console.log(value);
+      var result = parseFloat(value);
+      return result.toFixed(2);
+    };
+
     const parse_datetime = (dateString, type) => {
       if (type == "date") {
         return date.formatDate(dateString, "DD-MM-YYYY");
@@ -281,6 +446,7 @@ export default defineComponent({
       columns,
       dataTable,
       parse_datetime,
+      parse_decimal,
       filter: ref(""),
       loadingScreen,
     };
@@ -292,4 +458,38 @@ export default defineComponent({
 .q-dialog__inner--minimized {
   padding-top: 0px !important;
 }
+</style>
+
+<style lang="sass">
+.my-sticky-header-last-column-table
+
+  td:nth-child(11), td:nth-child(12), td:nth-child(13)
+    /* bg color is important for td; just specify one */
+    background-color: #f7f7f7
+
+.my-sticky-header-table
+  /* height or max-height is important */
+  height: 310px
+
+  .q-table__top,
+  .q-table__bottom,
+  thead tr:first-child th
+    /* bg color is important for th; just specify one */
+    background-color: white
+
+  thead tr th
+    position: sticky
+    z-index: 1
+  thead tr:first-child th
+    top: 0
+
+  /* this is when the loading indicator appears */
+  &.q-table--loading thead tr:last-child th
+    /* height of all previous header rows */
+    top: 48px
+
+  /* prevent scrolling behind sticky top row on focus */
+  tbody
+    /* height of all previous header rows */
+    scroll-margin-top: 48px
 </style>
